ed5acedbffbc1cc1aa8a8a1a703972d56ffb4b90
phoenixOfEther <david.oberkalmsteiner@hotmail.com>
2022-07-14 17:47:52 +0200

    added logger and better errorhandling

--- bin/git_clone_update_repos.py
+++ bin/git_clone_update_repos.py
@@ -10,8 +10,21 @@ __date__ = '2022/07/04'
 import argparse
 import os
 import configparser
-os.environ["GIT_PYTHON_REFRESH"] = "quiet"
+import shutil
+#os.environ["GIT_PYTHON_REFRESH"] = "quiet"
 import git
+from log_script import log
+import errno
+import stat
+
+
+def handleRemoveReadonly(func, path, exc):
+    excvalue = exc[1]
+    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
+        func(path)
+    else:
+        raise
 
 
 def is_valid_file(parser, arg):
@@ -27,8 +40,8 @@ parser.add_argument('-c', '--config_path', required=True, type=lambda x: is_vali
                     help='Pfad zu deinem Konfigurationsfile', metavar="FILE")
 parser.add_argument('-i', '--input_file_path', required=True, type=lambda x: is_valid_file(parser, x),
                     help='Pfad zu deinem File, in dem du die GitRepos definiert hast', metavar="FILE")
-parser.add_argument('-b', '--base_dir_path', required=True, type=lambda x: is_valid_file(parser, x),
-                    help='Pfad zu deinem Base-Verzeichnis, wo die GitRepos hingehören', metavar="FILE")
+parser.add_argument('-b', '--base_dir_path', required=True, type=str,
+                    help='Pfad zu deinem Base-Verzeichnis, wo die GitRepos hingehören', metavar="FOLDER")
 
 args = parser.parse_args()
 
@@ -38,19 +51,37 @@ config.sections()
 config.read(args.config_path)
 log_path = config['Config']['log_path']
 
+# ETHER CLONE OR PULL REPOS FROM LINKS
+folderNames = []
+
 file = open(args.input_file_path, "r", encoding='utf-8')
 for line in file:
     (githubLink, folderName) = line.split(' ')
+    folderNames.append(folderName)
     folderPath = os.path.join(args.base_dir_path, folderName).strip()
 
+    print(folderPath)
     if(os.path.exists(folderPath)):
+        # os.chdir(folderPath)
         repo = git.Repo(folderPath)
         o = repo.remotes.origin
         o.pull()
+        log('Pulled ' + folderPath)
+        repo.__del__()
         continue
     else:
-        # print(os.path.normpath(folderPath))
-        # print('C:\\temp\\Test\\Jerome_Fromherz_Noel_Küng')
-        # 'C:\\temp\\Test\\Jerome_Fromherz_Noel_Küng'
         os.makedirs(folderPath)
+        log('Created ' + folderPath)
     repo = git.Repo.clone_from(githubLink, folderPath)
+    log('Cloned ' + folderPath + ' from ' + githubLink)
+
+# DELETE OTHER/OLD DIRECTORIES
+dirs = os.listdir(args.base_dir_path)
+print(dirs)
+print(folderNames)
+for dir in dirs:
+    dirName = dir.strip()
+    if not dirName in folderNames:
+        shutil.rmtree(os.path.join(args.base_dir_path, dirName),
+                      ignore_errors=False, onerror=handleRemoveReadonly)
+        log('Deleted ' + dirName)
--- docs/Betriebsdokumentation_script1.md
+++ docs/Betriebsdokumentation_script1.md
@@ -28,6 +28,7 @@ TODO: Wie sind User-Home-Templates einzurichten
 TODO: Erzeugen der Input-Files beschreiben, falls noetig
 
 TODO: beschreiben des Scriptaufruf
+python .\git_clone_update_repos.py -c .\git_clone_update_repos_config.txt -i .\git_clone_update_repos_input_file.txt -b C:\temp\Test
 
 TODO: beschreiben der erzeugt files (falls solche erzeugt werden)
 
